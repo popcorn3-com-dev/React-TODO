ee433be527cea06ba278c1e6c48184276119f303
popcorn3-com-dev <shos3104@gmail.com>
2025-07-08 17:27:55 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+export const App = () => {
+  return (
+    <>
+      <h1>Hello, world</h1>
+      <p>よろしく</p>
+      {console.log('hoge!')}
+      <button onClick={() => alert()}>ボタン</button>
+    </>
+  );
+};
