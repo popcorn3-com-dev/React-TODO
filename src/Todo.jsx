f16283aea5972e53b8c8048ddaa36bda6b27d34c
popcorn3-com-dev <shos3104@gmail.com>
2025-07-14 18:33:40 +0900

    html, css

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,6 +1,48 @@
+import './style.css';
+
 export const Todo = () => {
   return (
-    <>hoge
-    </>
+    <div className='todo'>
+      <div className='input-area'>
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div className='incomplete-area'>
+        <p className='title'>未完了のTODO</p>
+        <ul>
+          <li>
+            <div className='list-row'>
+              <p className='todo-item'>TODOです</p>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
+          </li>
+          <li>
+            <div className='list-row'>
+              <p className='todo-item'>TODOです</p>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div className='complete-area'>
+        <p className='title'>完了のTODO</p>
+        <ul>
+          <li>
+            <div className='list-row'>
+              <p className='todo-item'>TODOでした</p>
+              <button>戻す</button>
+            </div>
+          </li>
+          <li>
+            <div className='list-row'>
+              <p className='todo-item'>TODOでした</p>
+              <button>戻す</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 };
