2d9f4f8c0604df091fce8b8b7840542c60cbc73e
popcorn3-com-dev <shos3104@gmail.com>
2025-07-15 17:47:20 +0900

    taskの削除機能

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -12,6 +12,11 @@ export const Todo = () => {
     setIncompleteTodos(newTodos);
     setTodoText('');
   }
+  const onClickDelete = (index) => {
+    const newTodos = [...incompleteTodos];
+    newTodos.splice(index, 1);
+    setIncompleteTodos(newTodos);
+  }
 
   return (
     <div className='todo'>
@@ -22,12 +27,12 @@ export const Todo = () => {
       <div className='incomplete-area'>
         <p className='title'>未完了のTODO</p>
         <ul>
-          {incompleteTodos.map((todo) => (
+          {incompleteTodos.map((todo, index) => (
             <li key={todo}>
               <div className='list-row'>
                 <p className='todo-item'>{todo}</p>
                 <button>完了</button>
-                <button>削除</button>
+                <button onClick={() => onClickDelete(index)}>削除</button>
               </div>
             </li>
           )
